574efba587d4d3c9fba98601f3104976f4f2ed13
felipetrontino <felipetrontino@gmail.com>
2020-02-06 20:40:17 -0300

    Define Discriminator as Type

--- Insurance.Core/Domain/Entities/ContractPart.cs
+++ Insurance.Core/Domain/Entities/ContractPart.cs
@@ -1,7 +1,11 @@
-﻿namespace Insurance.Core.Domain.Entities
+﻿using Insurance.Core.Domain.Enums;
+
+namespace Insurance.Core.Domain.Entities
 {
     public class ContractPart : BaseEntity
     {
+        public ContractPartType Type { get; set; }
+
         public string Name { get; set; }
 
         public string Address { get; set; }
--- Insurance.Core/Infra/Data/InsuranceDb.cs
+++ Insurance.Core/Infra/Data/InsuranceDb.cs
@@ -1,4 +1,5 @@
 ﻿using Insurance.Core.Domain.Entities;
+using Insurance.Core.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics.CodeAnalysis;
 
@@ -10,6 +11,16 @@ namespace Insurance.Core.Infra.Data
         public InsuranceDb(DbContextOptions options)
             : base(options)
         { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ContractPart>()
+                .HasDiscriminator(c => c.Type)
+                 .HasValue<ContractPart>(ContractPartType.Undefined)
+                .HasValue<Advisor>(ContractPartType.Advisor)
+                .HasValue<Carrier>(ContractPartType.Carrier)
+                .HasValue<Mga>(ContractPartType.Mga);
+        }
 
         public DbSet<ContractPart> ContractParts { get; set; }
 
